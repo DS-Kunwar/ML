d24611463db54856fb0645aef6dd2073569243c9
sourav <sourav.kunwar@gmail.com>
2020-09-04 11:54:00 +0530

    Created The basic MNIST Model from tensorflow tutorial

---
+++ fashion.py
@@ -0,0 +1,29 @@
+import absl
+import tensorflow as tf
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
+def _build_model() -> tf.keras.Model:
+    model = tf.keras.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    return model
+
+
+if __name__ == '__main__':
+    absl.logging.set_verbosity(absl.logging.INFO)
+    model = _build_model()
+    model.fit(train_images, train_labels, epochs=10)
