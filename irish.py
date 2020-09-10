fe7133136a8b409e45424869cf6c87a021613cb5
sourav <sourav.kunwar@gmail.com>
2020-09-10 20:47:23 +0530

    added build model funtion

--- irish.py
+++ irish.py
@@ -2,6 +2,14 @@ import os
 import pandas as pd 
 import tensorflow as tf 
 
+def get_model():
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.InputLayer(input_shape=(28,28))
+    ])
+
+    return model
+
 if __name__ == '__main__':
     print('Tensorflow version in {}'.format(tf.__version__))
-    
+    model = get_model()
